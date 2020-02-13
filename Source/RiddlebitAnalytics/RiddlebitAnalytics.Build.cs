12dfd3b269636f74aa66ee26706ad0ffde682f85
Jonathan@Work-Work <jona.jorg@gmail.com>
2020-02-13 15:25:37 +0100

    Minimal plugin source code

---
+++ Source/RiddlebitAnalytics/RiddlebitAnalytics.Build.cs
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class RiddlebitAnalytics : ModuleRules
+{
+	public RiddlebitAnalytics(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicIncludePaths.AddRange(new string[] {});
+		PrivateIncludePaths.AddRange(new string[] {});
+		
+		PublicDependencyModuleNames.AddRange(new string[]{"Core"});
+		PrivateDependencyModuleNames.AddRange(new string[]{"CoreUObject", "Engine", "Slate", "SlateCore",});
+		
+		DynamicallyLoadedModuleNames.AddRange(new string[]{});
+	}
+}
