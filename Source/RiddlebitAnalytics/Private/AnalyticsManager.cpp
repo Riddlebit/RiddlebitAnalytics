02523052245360c8f2610a980d21cb09e9275b3f
Jonathan@Work-Work <jona.jorg@gmail.com>
2020-02-14 17:14:04 +0100

    Batch analytics and clear the buffer

--- Source/RiddlebitAnalytics/Private/AnalyticsManager.cpp
+++ Source/RiddlebitAnalytics/Private/AnalyticsManager.cpp
@@ -5,7 +5,6 @@ UAnalyticsManager* UAnalyticsManager::Instance = NULL;
 
 void UAnalyticsManager::InitAnalytics()
 {
-	UE_LOG(LogTemp, Warning, TEXT("Makka"));
 	Http = &FHttpModule::Get();
 	Instance = NewObject<UAnalyticsManager>();
 }
@@ -17,14 +16,20 @@ void UAnalyticsManager::RegisterAnalytics(UAnalyticsData* Data)
 
 void UAnalyticsManager::PushAnalytics()
 {
-	if (Http != NULL) {
+	if (Http != NULL && Instance->Buffer.Num() > 0) {
 		TSharedRef<IHttpRequest> Request = Http->CreateRequest();
 		Request->SetURL("http://92.62.46.171:3000/api/ui-events");
 		Request->SetVerb("POST");
 		Request->SetHeader(TEXT("User-Agent"), "X-UnrealEngine-Agent");
 		Request->SetHeader("Content-Type", TEXT("application/json"));
-		Request->SetContentAsString("[ " + Instance->Buffer[0]->ToJsonString() + " ]");
+		FString Content = "[ ";
+		for (int i = 0; i < Instance->Buffer.Num() - 1; ++i) {
+			Content += Instance->Buffer[i]->ToJsonString() + ", ";
+		}
+		Content += Instance->Buffer[Instance->Buffer.Num() - 1]->ToJsonString() + " ]";
+		Request->SetContentAsString(Content);
 		Request->ProcessRequest();
+		Instance->Buffer.Empty();
 	}
 	else {
 		UE_LOG(LogTemp, Warning, TEXT("Please call InitAnalytics() before anything else."));
@@ -38,7 +43,6 @@ UAnalyticsData* UAnalyticsData::MakeAnalyticsData() {
 UAnalyticsData* UAnalyticsData::AddField(FString Key, FString Value)
 {
 	Fields.Add(Key, Value);
-	UE_LOG(LogTemp, Warning, TEXT("Pakka %s %s"), *Key, *Value);
 	return this;
 }
 
@@ -47,16 +51,14 @@ FString UAnalyticsData::ToJsonString()
 	TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject);
 	JsonObject->SetStringField("timestamp", TimeStamp);
 
-	UE_LOG(LogTemp, Warning, TEXT("hei %i"), Fields.Num());
-
 	for (auto& Elem : Fields) {
 		JsonObject->SetStringField(Elem.Key, Elem.Value);
-		UE_LOG(LogTemp, Warning, TEXT("Kukki %s %s"), *Elem.Key, *Elem.Value);
 	}
 	FString OutputString;
+
 	TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&OutputString);
 	FJsonSerializer::Serialize(JsonObject.ToSharedRef(), Writer);
-	UE_LOG(LogTemp, Warning, TEXT("json %s"), *OutputString);
+
 	return OutputString;
 }
 
@@ -64,11 +66,10 @@ UAnalyticsData::UAnalyticsData()
 {
 	FString UnixTime = FString::FromInt(FDateTime::UtcNow().ToUnixTimestamp());
 	FString MilliSeconds = FString::FromInt(FDateTime::UtcNow().GetMillisecond());
-	FVector Vec;
+
 	int ZeroesToPrepend = 3 - MilliSeconds.Len();
 	for (int i = 0; i < ZeroesToPrepend; ++i)
 		MilliSeconds = "0" + MilliSeconds;
 
 	TimeStamp = UnixTime + MilliSeconds;
-	UE_LOG(LogTemp, Warning, TEXT("Time = %s"), *TimeStamp);
 }
