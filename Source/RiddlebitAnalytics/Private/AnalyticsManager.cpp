ac86cce324974afc7afb7696951401741fca6dc3
Jonathan@Work-Work <jona.jorg@gmail.com>
2020-02-14 15:50:11 +0100

    Implement analytic posting

--- Source/RiddlebitAnalytics/Private/AnalyticsManager.cpp
+++ Source/RiddlebitAnalytics/Private/AnalyticsManager.cpp
@@ -1,11 +1,18 @@
 #include "..\Public\AnalyticsManager.h"
 
 FHttpModule* UAnalyticsManager::Http = NULL;
+UAnalyticsManager* UAnalyticsManager::Instance = NULL;
 
 void UAnalyticsManager::InitAnalytics()
 {
 	UE_LOG(LogTemp, Warning, TEXT("Makka"));
 	Http = &FHttpModule::Get();
+	Instance = NewObject<UAnalyticsManager>();
+}
+
+void UAnalyticsManager::RegisterAnalytics(UAnalyticsData* Data)
+{
+	Instance->Buffer.Add(Data);
 }
 
 void UAnalyticsManager::PushAnalytics()
@@ -16,10 +23,52 @@ void UAnalyticsManager::PushAnalytics()
 		Request->SetVerb("POST");
 		Request->SetHeader(TEXT("User-Agent"), "X-UnrealEngine-Agent");
 		Request->SetHeader("Content-Type", TEXT("application/json"));
-		Request->SetContentAsString("{ \"id\" : \"havi\" }");
+		Request->SetContentAsString("[ " + Instance->Buffer[0]->ToJsonString() + " ]");
 		Request->ProcessRequest();
 	}
 	else {
 		UE_LOG(LogTemp, Warning, TEXT("Please call InitAnalytics() before anything else."));
 	}
+}
+
+UAnalyticsData* UAnalyticsData::MakeAnalyticsData() {
+	return NewObject<UAnalyticsData>();
+}
+
+UAnalyticsData* UAnalyticsData::AddField(FString Key, FString Value)
+{
+	Fields.Add(Key, Value);
+	UE_LOG(LogTemp, Warning, TEXT("Pakka %s %s"), *Key, *Value);
+	return this;
+}
+
+FString UAnalyticsData::ToJsonString()
+{
+	TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject);
+	JsonObject->SetStringField("timestamp", TimeStamp);
+
+	UE_LOG(LogTemp, Warning, TEXT("hei %i"), Fields.Num());
+
+	for (auto& Elem : Fields) {
+		JsonObject->SetStringField(Elem.Key, Elem.Value);
+		UE_LOG(LogTemp, Warning, TEXT("Kukki %s %s"), *Elem.Key, *Elem.Value);
+	}
+	FString OutputString;
+	TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&OutputString);
+	FJsonSerializer::Serialize(JsonObject.ToSharedRef(), Writer);
+	UE_LOG(LogTemp, Warning, TEXT("json %s"), *OutputString);
+	return OutputString;
+}
+
+UAnalyticsData::UAnalyticsData()
+{
+	FString UnixTime = FString::FromInt(FDateTime::UtcNow().ToUnixTimestamp());
+	FString MilliSeconds = FString::FromInt(FDateTime::UtcNow().GetMillisecond());
+	FVector Vec;
+	int ZeroesToPrepend = 3 - MilliSeconds.Len();
+	for (int i = 0; i < ZeroesToPrepend; ++i)
+		MilliSeconds = "0" + MilliSeconds;
+
+	TimeStamp = UnixTime + MilliSeconds;
+	UE_LOG(LogTemp, Warning, TEXT("Time = %s"), *TimeStamp);
 }
--- Source/RiddlebitAnalytics/Public/AnalyticsManager.h
+++ Source/RiddlebitAnalytics/Public/AnalyticsManager.h
@@ -4,27 +4,57 @@
 
 //#include "Runtime/Online/HTTP/Public/Http.h"
 #include "Http.h"
-//#include "Dom/JsonObject.h"
-//#include "Serialization/JsonReader.h"
-//#include "Serialization/JsonSerializer.h"
+
+
+#include "Misc/DateTime.h" 
+#include "Dom/JsonObject.h"
+#include "Serialization/JsonReader.h"
+#include "Serialization/JsonSerializer.h"
 
 #include "AnalyticsManager.generated.h"
 
 UCLASS()
-
 class RIDDLEBITANALYTICS_API UAnalyticsManager : public UObject
 {
 	GENERATED_BODY()
 
-public:	
+public:
 
 	UFUNCTION(BlueprintCallable, Category = "RiddlebitAnalytics")
 	static void InitAnalytics();
 
 	UFUNCTION(BlueprintCallable, Category = "RiddlebitAnalytics")
+	static void RegisterAnalytics(UAnalyticsData* Data);
+
+	UFUNCTION(BlueprintCallable, Category = "RiddlebitAnalytics")
 	static void PushAnalytics();
+
+	TArray<UAnalyticsData*> Buffer;
 
 private:
 	static FHttpModule* Http;
+	static UAnalyticsManager* Instance;
 };
 
+UCLASS(Blueprintable, BlueprintType)
+class RIDDLEBITANALYTICS_API UAnalyticsData : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintPure, Category = "RiddlebitAnalytics")
+	static UAnalyticsData* MakeAnalyticsData();
+	
+	UFUNCTION(BlueprintPure, Category = "RiddlebitAnalytics")
+	UAnalyticsData* AddField(FString Key, FString Data);
+
+	FString ToJsonString();
+
+	UAnalyticsData();
+
+private:
+	FString TimeStamp;
+
+	TMap<FString, FString> Fields;
+};
